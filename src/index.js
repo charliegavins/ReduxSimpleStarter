37917d24eb40a9721bab3b1db8f12b5f2db6f803
gabrielghost <charlie.gavins@neonseed.com>
2017-08-30 19:26:05 +0100

    section 2 lecture 32

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyB6_7a8G2VCpgjpU8SbUV_cv-wg1dj-Uoo';
 
-const API_KEY = 'AIzaSyCih-OEBGmQZWEc3sruFfFuWxTIIDQThXQ';
+class App extends Component{
+  constructor(props){
+    super(props);
 
-// Create a new component and this component should produce some HTML
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-);
+    this.videoSearch('');
+  }
+
+videoSearch(term){
+  YTSearch({key: API_KEY, term: term}, videos => {
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+    });
+  });
 }
 
-// Once HTML is created, this should inject this into the DOM
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        videos={this.state.videos} />
+      </div>
+  );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
